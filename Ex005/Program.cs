b09a3373a2e7bca2133ad07f3b38728ff9b68e86
Mateus Freitas <150572494+MateusDeFreitas@users.noreply.github.com>
2025-01-15 22:28:07 -0300

    Adicionando exercício 5

---
+++ Ex005/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Ex005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Insira a velocidade do carro par validação: ");
+                double velocidade = Convert.ToDouble(Console.ReadLine());
+
+                if (velocidade > 80)
+                {
+                    Console.WriteLine($"Esse carro foi multado em {7 * (velocidade - 80)} reais.");
+                }
+                else
+                {
+                    Console.WriteLine("O carro está dirigindo dentro da lei! Parabéns");
+                }
+
+
+                Console.Write("Você quer analisar outro carro?: ");
+                string resposta = Console.ReadLine();
+                
+                if (resposta == "sim")
+                {
+                    
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}

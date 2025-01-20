426cb8d0cb60ab06a06c9a97942c3a9a349cf7df
Mateus Freitas <150572494+MateusDeFreitas@users.noreply.github.com>
2025-01-20 19:09:47 -0300

    Adicionado arquivos do exercício 7
    - Um código que testa se um valor é par ou impar

---
+++ Ex007/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Ex007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("------ Vamos testar se o número é par -------");
+            Console.WriteLine("---------------------------------------------\n\n");
+            while (true)
+            {
+                Console.Write("\nDigite um número: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine("Esse número é par");
+                    Console.WriteLine("---------------------------------------------\n\n");
+                }
+                else if (numero % 2 == 1)
+                {
+                    Console.WriteLine("Esse número é impar");
+                    Console.WriteLine("---------------------------------------------\n\n");
+                }
+
+
+                string resposta = "";
+                while (resposta != "SIM" || resposta != "NÃO")
+                {
+                    Console.Write("Você quer testar outro número? ");
+                    resposta = Console.ReadLine().ToUpper();
+
+                    if (resposta == "SIM")
+                    {
+                        Console.WriteLine("\nVamos testar mais um número!\n");
+                        break;
+                    }
+                    else if (resposta == "NÃO")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Insira um valor válido.");
+                    }
+                }
+                if (resposta == "NÃO")
+                {
+                    Console.WriteLine("\nFinalizando Programa");
+                    break;
+                }
+
+            }
+        }
+    }
+}
+

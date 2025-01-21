4409aa6e8276df004d11ef67887e8667b419200a
Mateus Freitas <150572494+MateusDeFreitas@users.noreply.github.com>
2025-01-20 21:37:03 -0300

    Atualização jogo dos números
    - Adição de dificuldade no jogo

--- Ex009/Program.cs
+++ Ex009/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Ex009
+﻿using System;
+
+namespace Ex009
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            while (true)
+            while (true) // while system
             {
                 Random random = new Random();
                 int randomNum = random.Next(1, 101); // from 1 to 100
@@ -12,14 +14,28 @@
                 int shot;
                 bool showRules = false;
                 int attempts = 0;
+                int difficulty;
 
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("------- Vamos jogar o jogo dos números -------");
+                Console.WriteLine("----------------------------------------------\n\n");
 
-                while (true)
+                Console.WriteLine("Escolha a dificuldade do jogo:");
+                Console.WriteLine("1. Fácil   - Você terá tentativas ilimitadas.");
+                Console.WriteLine("2. Normal  - Você terá 10 tentativas para acertar o número.");
+                Console.WriteLine("3. Difícil - Você terá apenas 5 tentativas para acertar o número e sem pistas.");
+                Console.Write("Digite o número correspondente à dificuldade escolhida (1, 2 ou 3): ");
+                difficulty = int.Parse(Console.ReadLine());
+
+                while (difficulty <= 0 || difficulty >= 4) // while difficulty selection
                 {
-                    Console.WriteLine("----------------------------------------------");
-                    Console.WriteLine("------- Vamos jogar o jogo dos números -------");
-                    Console.WriteLine("----------------------------------------------\n\n");
-                    
+                    Console.Write("Digite uma dificuldade válida (1, 2 ou 3): ");
+                    difficulty = int.Parse(Console.ReadLine());
+                }
+
+                while (true) // while game
+                {
+
                     if (!showRules)
                     {
                         Console.WriteLine("\nBem-vindo ao jogo 'Adivinhe o Número'!");
@@ -32,6 +48,19 @@
                         Console.WriteLine("5. Quando você acertar, o jogo mostrará quantas tentativas foram necessárias.");
                         Console.WriteLine("6. Boa sorte e divirta-se!\n\n");
                         showRules = true;
+                    }
+
+                    if (difficulty == 2 && attempts > 9)
+                    {
+                        Console.WriteLine($"\n--- DERROTA ---");
+                        Console.WriteLine($"\nVocê excedeu o limite de tentativas da sua dificuldade ({attempts}).");
+                        break;
+                    }
+                    else if (difficulty == 3 && attempts > 4)
+                    {
+                        Console.WriteLine($"\n--- DERROTA ---");
+                        Console.WriteLine($"\nVocê excedeu o limite de tentativas da sua dificuldade ({attempts}).");
+                        break;
                     }
 
                     Console.WriteLine("Escolha um número entre 1 a 100");
@@ -48,32 +77,38 @@
                     else if (shot > randomNum)
                     {
                         Console.WriteLine($"\nO número secreto é menor do que o seu chute!");
-                        if (shot - randomNum <= 10)
+                        if (difficulty != 3)
                         {
-                            Console.WriteLine($"\nVocê está quente!");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"\nVocê está frio!");
+                            if (shot - randomNum <= 10)
+                            {
+                                Console.WriteLine($"\nPista: Você está quente!");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"\nPista: Você está frio!");
+                            }
                         }
                     }
                     else if (shot < randomNum)
                     {
                         Console.WriteLine($"\nO número secreto é maior do que o seu chute!");
-                        if (randomNum - shot <= 10)
+                        if (difficulty != 3)
                         {
-                            Console.WriteLine($"\nVocê está quente!");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"\nVocê está frio!");
+                            if (randomNum - shot <= 10)
+                            {
+                                Console.WriteLine($"\nPista: Você está quente!");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"\nPista: Você está frio!");
+                            }
                         }
                     }
                     Console.WriteLine($"\nEstá foi a {attempts}° tentativa.");
                 }
 
                 string resposta = "";
-                while (resposta != "SIM" || resposta != "NÃO")
+                while (resposta != "SIM" || resposta != "NÃO") // while to play again
                 {
                     Console.Write("Você quer jogar novamente? ");
                     resposta = Console.ReadLine().ToUpper();
